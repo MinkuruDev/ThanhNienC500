05f856a28d34e4110b8de3bab851e7812fb88f68
Mars <52197560+MinkuruDev@users.noreply.github.com>
2022-05-20 16:41:08 +0700

    add hide nav bar to EVERY pages

--- assets/js/Nav.js
+++ assets/js/Nav.js
@@ -1,9 +1,17 @@
 const icon = document.querySelector('.hamburg');
 const link = document.querySelector('.header__navbar-list');
+// get nav button id nav-toggle
+const navToggle = document.getElementById('nav-toggle');
 
 // Added click event listener to icon hamburg
 icon.addEventListener('click', ()=>{
    if(link.style.display == 'flex' || link.style.display == '')
     link.style.display = 'none';
    else link.style.display = 'flex'
-})
+});
+
+function hideNav() {
+    // nav toggle is checkbox, uncheck it
+   navToggle.checked = false;
+   return true;
+}

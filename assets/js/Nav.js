7755d522a7f5d2e9534a3ad8069cdbdbc3c726e4
Mars <52197560+MinkuruDev@users.noreply.github.com>
2022-05-20 16:54:02 +0700

    fix hide nav

--- assets/js/Nav.js
+++ assets/js/Nav.js
@@ -3,15 +3,15 @@ const link = document.querySelector('.header__navbar-list');
 // get nav button id nav-toggle
 const navToggle = document.getElementById('nav-toggle');
 
+function hideNav() {
+   // nav toggle is checkbox, uncheck it
+  navToggle.checked = false;
+  return true;
+}
+
 // Added click event listener to icon hamburg
 icon.addEventListener('click', ()=>{
    if(link.style.display == 'flex' || link.style.display == '')
     link.style.display = 'none';
    else link.style.display = 'flex'
 });
-
-function hideNav() {
-    // nav toggle is checkbox, uncheck it
-   navToggle.checked = false;
-   return true;
-}
